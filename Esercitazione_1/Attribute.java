efd468655b837e4f1b95343f2ec50330e19066d0
Vsb23 <v.birardi3@studenti.uniba.it>
2023-04-29 10:58:30 +0200

    Commento iniziale

---
+++ Esercitazione_1/Attribute.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+
+
+abstract class Attribute {
+    private String name;
+    private int index;
+    protected  Attribute(String name, int index){
+        this.name = name;
+        this.index = index;
+    }
+
+    protected String getName(){
+        return this.name;
+    }
+
+    protected int getIndex(){
+        return this.index;
+    }
+
+    public String toString(){
+        return this.name;
+    }
+}
+
+
