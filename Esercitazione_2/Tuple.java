d7523d1ac4f0f3a6e917aa182a9ce360d82f630a
P3ppe01 <g.durso15@studenti.uniba.it>
2023-05-01 11:58:25 +0200

    modified:   Esercitazione_2/Tuple.java

--- Esercitazione_2/Tuple.java
+++ Esercitazione_2/Tuple.java
@@ -1,3 +1,25 @@
+
 public class Tuple {
-    
+    private Item[] tuple;
+
+    Tuple(int size) {
+        Item[] tuple = new Item[size];
+    }
+
+    private int getLenght() {
+        return tuple.length;
+    }
+
+    private Item get(int i) {
+        return tuple[i];
+    }
+
+    private void add(Item c, int i) {
+        tuple[i] = c;
+    }
+
+    private double getDistance(Tuple obj) {
+
+    }
+
 }
