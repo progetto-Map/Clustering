7dacb8b27917052c2b6c543f3b137ad91a7e0b7a
P3ppe01 <g.durso15@studenti.uniba.it>
2023-05-04 17:53:25 +0200

    new file:   Esercitazione_2/ClusterSet.java

---
+++ Esercitazione_2/ClusterSet.java
@@ -0,0 +1,24 @@
+private class ClusterSet {
+    Cluster C[];
+    int i = 0; // posizione valida per la memorizzazione di un nuovo cluster in C
+
+    /**
+     * COMPORTAMENTO: creo l'oggetto array riferito da C
+     * 
+     * @param k Numero di cluster da generare(k-means)
+     */
+    ClusterSet(int k) {
+
+    }
+
+    /**
+     * COMPORTAMENTIO: assegna c a C[i] e incrementa i
+     * 
+     * @param c
+     */
+    protected void add(Cluster c) {
+
+    }
+
+    // DA FINIRE TUTTO PERCHè ERA SOLO UN ABBOZZO
+}
