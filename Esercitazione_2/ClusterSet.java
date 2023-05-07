cff978a3b07f2c2e9dd8fea2972027702c5a648b
Alessandro <a.carli3@studenti.uniba.it>
2023-05-07 12:06:39 +0200

    modified:   Esercitazione_2/ClusterSet.java

--- Esercitazione_2/ClusterSet.java
+++ Esercitazione_2/ClusterSet.java
@@ -8,7 +8,7 @@ private class ClusterSet {
      * @param k Numero di cluster da generare(k-means)
      */
     ClusterSet(int k) {
-
+        
     }
 
     /**
@@ -16,8 +16,54 @@ private class ClusterSet {
      * 
      * @param c
      */
-    protected void add(Cluster c) {
+    private void add(Cluster c) {
+        C[i] = c;
+        i++; 
+    }
 
+    private Cluster get(int i){
+        return C[i];
+    }
+
+    private void initializeCentroids(Data data){
+        int centroidIndexes[]=data.sampling(C.length);
+        for (int i=0;i<centroidIndexes.length;i++)
+        {
+            Tuple centroidI=data.getItemSet(centroidIndexes[i]);
+            add(new Cluster(centroidI));
+        }
+    }
+
+    private Cluster nearesCluster(Tuple tuple){
+
+    }
+
+    private Cluster currentCluster(int id){
+        for (int i=0;i<C.length;i++){
+            if (C[i].contain(id))
+            return C[i];
+        }
+        return null;
+    }
+
+    private void updateCentroids(Data data){
+        for(int i=0;i<C.lenght;i++){
+            C[i].computeCentroid(data);
+        }
+    }
+
+    public String toString(){
+        
+    }
+
+    public String toString(Data data){
+        String str="";
+        for(int i=0;i<C.lenght;i++){
+            if (C[i]!=null){
+                str+=i+":"+C[i].toString(data)+"\n";
+            }
+        }
+        return str;
     }
 
     // DA FINIRE TUTTO PERCHè ERA SOLO UN ABBOZZO
