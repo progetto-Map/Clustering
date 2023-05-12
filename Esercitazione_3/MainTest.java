d79e08b09b21ba3cbb45be7b54889e59fce2055e
Alessandro <a.carli3@studenti.uniba.it>
2023-05-12 16:18:00 +0200

    modified:   Esercitazione_3/MainTest.java

--- Esercitazione_3/MainTest.java
+++ Esercitazione_3/MainTest.java
@@ -6,12 +6,17 @@ public class MainTest {
 	 */
 	public static void main(String[] args) {
 		
-		
+		boolean errore;
 		Data data =new Data();
 		System.out.println(data);
-		int k=3;
-		KmeansMiner kmeans=new KmeansMiner(k);
-		int numIter=kmeans.kmeans(data);
+		int k=0;
+		KMeansMiner kmeans = null;
+		int numIter=0;
+		do {
+			System.out.println("Inserisci il numero di k:");
+			errore=false;
+			k=Keyboard.readInt();
+		}while (errore);
 		System.out.println("Numero di Iterazione:"+numIter);
 		System.out.println(kmeans.getC().toString(data));
 		
@@ -19,3 +24,4 @@ public class MainTest {
 	}
 
 }
+
